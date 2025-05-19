40dafb0b089421c1c1b8c36b44bc494a64425bca
Adelknf <157009196+Adelknf@users.noreply.github.com>
2025-05-19 19:56:44 +0200

    Update Données_jour.py

--- Données_jour.py
+++ Données_jour.py
@@ -21,8 +21,8 @@ df["Heure"] = pd.to_datetime(df["Heure"], format="%Y-%m-%d %H:%M:%S")
 
 # Filtrer entre 9h00 et 17h30
 today = datetime.date.today()
-start_time = datetime.datetime.combine(today, datetime.time(9, 0))
-end_time = datetime.datetime.combine(today, datetime.time(17, 30))
+start_time = datetime.datetime.combine(today, datetime.time(7, 0))
+end_time = datetime.datetime.combine(today, datetime.time(15, 30))
 
 df_filtered = df[(df["Heure"] >= start_time) & (df["Heure"] <= end_time)]
 
